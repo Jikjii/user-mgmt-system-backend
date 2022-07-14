dd82881fb9eb0ffe7cab274b002498cb7d5e4a23
geraldo <85137475+Jikjii@users.noreply.github.com>
2022-07-14 16:17:20 -0400

    Initial Commit

---
+++ src/test/java/com/jikjii/usermgmtsystem/UserMgmtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jikjii.usermgmtsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMgmtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
